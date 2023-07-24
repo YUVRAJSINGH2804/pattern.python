d7d0276c9bfe8ebe783fa320ca0da8733ebd682d
YUVRAJ SINGH <117624870+YUVRAJSINGH2804@users.noreply.github.com>
2023-07-24 17:38:35 +0530

    Update pattern_python.py

--- pattern_python.py
+++ pattern_python.py
@@ -1,4 +1,21 @@
+#1st
+print("1st")
+for i in range(1,5):
+    for j in range(1,5):
+        print(i, end=' ')
+    print()
+
+#2nd
+print("2nd")
 for i in range(1,5):
     for j in range (1,5):
         print(j,end=" ")
     print(" ")
+
+#3rd
+print("3rd")
+for i in range(1,5):
+    for j in range (4,0,-1):
+        print(j,end=" ")
+    print(" ")
+
